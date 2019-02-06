1374d9246ccdfad87f4b693be2ae0dddd38b5ceb
Robert <robert.pakkanen@helsinki.fi>
2019-02-06 14:24:27 +0200

    added static

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('data', (req, res) => {
   const data = JSON.stringify(req.body)
