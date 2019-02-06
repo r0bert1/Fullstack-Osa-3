950e00a082baeaa5490fac619b9ae9d70695efdf
Robert <robert.pakkanen@helsinki.fi>
2019-02-06 14:20:13 +0200

    added build

---
+++ build/precache-manifest.12941246fcb5887225a79e016ea22bf0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b3d2afbbd30dd32e2f3",
+    "url": "/static/js/main.7b3d2afb.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "1fe1e80da9d61e4b17c9ea8d2e651e70",
+    "url": "/index.html"
+  }
+];
